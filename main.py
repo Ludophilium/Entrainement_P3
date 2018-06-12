fa794fea1cfd622811073c311d0b419c4aefea31
Ludophilium <nyaritakunai@outlook.com>
2018-06-12 04:36:45 +0200

    Changement graphique pour se rapprocher d'une Atari 2600. Ajout de nouvelles fonctionnalités : McGuyver a maintenant un sprite pour chaque direction. Le garde parait plus vivant et surveille la sortie. Pour gagner, il ne suffit plus de toucher le garde, mais il faut le surprendre

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ class Fenetre :
     RESOLUTION = [15*COTE_SPRITE,15*COTE_SPRITE] 
     ICONE_PTH = pygame.image.load("sprites/mur.png") 
 
-    FENETRE = pygame.display.set_mode(RESOLUTION, pygame.RESIZABLE) 
+    FENETRE = pygame.display.set_mode(RESOLUTION) 
     TITLE_FEN = pygame.display.set_caption(TITRE)
     ICONE_FEN = pygame.display.set_icon(ICONE_PTH.convert_alpha())
 
@@ -25,8 +25,8 @@ class Niveau :
     
     def __init__(self, textver_pth) :
         self.textver_pth = textver_pth
-        self.fond = pygame.image.load("sprites/fond.jpg")
-        self.sprite_mur = pygame.image.load("sprites/mur.png") 
+        self.fond = pygame.image.load("sprites/fond_n.png")
+        self.sprite_mur = pygame.image.load("sprites/mur2.png") 
     
     @property
     def listver (self) : 
@@ -49,11 +49,13 @@ class Niveau :
 
 class Personnage : 
   
-    def __init__(self, sprite_pth, position) :
-        self.sprite = pygame.image.load(sprite_pth)
+    def __init__(self, sprite_pth, position, orientation) :
         self.position = position 
+        self.sprite = pygame.image.load(sprite_pth)
         self.ko = False
-        
+        self.orientation = orientation
+        self.count = 0
+              
     @property
     def position_pf(self) : 
         return list(x*Fenetre.COTE_SPRITE for x in self.position)
@@ -66,21 +68,35 @@ class Personnage :
     def Y (self) : 
         return self.position[1] 
 
+    def orienter (self) :
+        self.count += 1
+
+        if self.count % 100 == 0 : 
+            if self.orientation == 'gauche' : 
+                self.sprite = pygame.transform.rotate (self.sprite, -90)
+                self.orientation = "haut" 
+            elif self.orientation == 'haut' :
+                self.sprite = pygame.transform.rotate(self.sprite, 90)
+                self.orientation = "gauche" 
+
     def afficher (self) : 
         sprite_pf = self.sprite.convert_alpha() 
         if self.ko == False :
             Fenetre.FENETRE.blit(sprite_pf, self.position_pf)       
 
 class Joueur (Personnage) : 
-    def __init__(self, sprite_pth, position, niveau) : 
-        super().__init__(sprite_pth, position) 
+    def __init__(self, spriteG_pth, spriteD_pth, spriteH_pth, spriteB_pth, position, orientation, niveau) : 
+        super().__init__(spriteD_pth, position, orientation) 
         self.objets = list()
-        self.nl = niveau.listver 
-
+        self.nl = niveau.listver
+        self.sprite_pthdc = dict(gauche = spriteG_pth, droite = spriteD_pth, haut = spriteH_pth, bas = spriteB_pth) 
     def deplacer(self, direction) : 
 
+        self.sprite = pygame.image.load(self.sprite_pthdc[direction])
+        self.orientation = direction
+
         if direction == 'droite' :
-        
+            
             if self.X+1<len(self.nl[0]) and self.nl[self.Y][self.X+1] != 'W' : 
                 self.position[0] += 1
 
@@ -122,18 +138,26 @@ class Joueur (Personnage) :
 
     def endormir (self, garde) : 
         if self.position == garde.position :
+            
             if sorted(self.objets) == [1,2,3] :
-                garde.ko = True
-                print("Geniaaal ! Tu as gagné ! Merci d'avoir joué")
-                #pygame.time.wait(3*1000)
-                pygame.quit()
-                sys.exit()
+                if self.orientation == 'droite' and garde.orientation == 'gauche' or self.orientation == 'bas' and garde.orientation == 'haut' :
+                    self.ko = True
+                    for x in range(15) :
+                        print("What... What have you done...")
+                        #pygame.time.wait(1*1000)
+                        #time.sleep(1)
+            
+                elif self.orientation == 'droite' and garde.orientation == 'haut' or self.orientation == 'bas' and garde.orientation == 'gauche' and sorted(self.objets) == [1,2,3] :
+                    garde.ko = True
+                    print("Geniaaal ! Tu as gagné ! Merci d'avoir joué")
+                    #pygame.time.wait(3*1000)
+                    pygame.quit()
+                    sys.exit()
             else : 
                 self.ko = True
                 for x in range(15) :
                     print("What... What have you done...")
-                #pygame.time.wait(1*1000)
-                #time.sleep(1)
+
 
 class Item : 
 
@@ -171,8 +195,8 @@ class Item :
 def main() :
 
     niveau_1 = Niveau("level_1.txt")
-    garde = Personnage("sprites/kd.png",[14,14]) 
-    heros = Joueur("sprites/dk.png",[0,0], niveau_1)
+    garde = Personnage("sprites/aku.png", [14,14], 'gauche') 
+    heros = Joueur("sprites/manL.png", "sprites/manR.png", "sprites/manU.png", "sprites/manD.png", [0,0], "droite", niveau_1)
     
     Item.positionner(3,niveau_1.listver)
 
@@ -180,7 +204,7 @@ def main() :
     item2 = Item(2, "sprites/item.png")
     item3 = Item(3, "sprites/item.png")
 
-    niveau_1.afficher() 
+    niveau_1.afficher()
 
     pygame.key.set_repeat(100, 25)
 
@@ -195,9 +219,9 @@ def main() :
 
             if evt.type == pygame.KEYDOWN and evt.key == pygame.K_DOWN : 
                 heros.deplacer("bas")
-               
+
             if evt.type == pygame.KEYDOWN and evt.key == pygame.K_RIGHT : 
-                heros.deplacer("droite")
+                heros.deplacer("droite")            
             
             if evt.type == pygame.KEYDOWN and evt.key == pygame.K_UP : 
                 heros.deplacer("haut")
@@ -206,6 +230,7 @@ def main() :
                 heros.deplacer("gauche")
 
         niveau_1.afficher() 
+        garde.orienter()
         garde.afficher() 
 
         heros.ramasser_objet(item1, item2, item3)
